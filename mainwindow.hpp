d2b7b00dd0a4c595f9f82ceacdf4dad08120f214
akosleepe <leolastleo@gmail.com>
2022-05-07 10:02:33 +0700

    first commit

---
+++ mainwindow.hpp
@@ -0,0 +1,26 @@
+#ifndef MAINWINDOW_HPP
+#define MAINWINDOW_HPP
+
+#include <QMainWindow>
+
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+public slots:
+    void generateKey_button();
+    void encode_button();
+    void decode_button();
+private:
+    Ui::MainWindow *ui;
+    uint8_t key[32];
+};
+#endif // MAINWINDOW_HPP
